2aaec7f5e254fca7a123156749806dee6f55a9ee
Kheifer Fuller and Trevor Gill <trevor@>
2017-07-03 10:01:47 -0700

    Basic homepage with adding function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2){
+  return number1+number2
+};
+
+number1=parseInt(prompt("What is your first number?"));
+number2=parseInt(prompt("What is your second number?"));
+
+alert(add(number1,number2));
